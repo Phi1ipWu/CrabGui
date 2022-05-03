d8d22d88c950b98ccc95da5317f513945baf22cb
Wu Yi <PhilipWu@live.com>
2022-05-03 22:05:49 +0800

    Tofu Effect

---
+++ Core/include/CrabGuiMesh.h
@@ -0,0 +1,56 @@
+﻿
+#ifndef __CrabGuiMesh_h__
+#define __CrabGuiMesh_h__
+
+
+namespace CrabGui
+{
+
+	/// 网格
+	class CRABGUIEXPORT Mesh
+	{
+	public:
+		Mesh();
+		Mesh(UInt uVertexNum, UInt uTriangleNum);
+		virtual ~Mesh();
+
+		/// 清除数据
+		void    clear();
+
+		/// 获取点个数
+		UInt 	getVertexNum();
+
+		/// 获取
+		const PointReal* getVertices();
+
+		/// 设置点个数
+		void 	setVertexNum(UInt uVertexNum);
+
+		/// 添加点
+		Bool 	setVertex(UInt uVertexIndex, const PointReal* pPoint, const PointReal* pTexPoint1, const PointReal* pTexPoint2);
+
+		/// 设置三角形个数
+		void 	setTriangleNum(UInt uTriangleNum);
+
+		/// 设置单个三角形
+		Bool 	setTriangle(UInt uTriangleIndex, Int16 sPoint1, Int16 sPoint2, Int16 sPoint3);
+
+
+	public:
+
+		
+
+
+
+	protected:
+		UInt  			_uVertexNum;
+		PointReal*		_pVertices;
+		PointReal*		_pTexVertices;
+
+		UInt 			_uTriangleNum;
+		Int16*			_pTriangles;
+	};
+
+}
+
+#endif	//	__CrabGuiMesh_h__
---
+++ Core/src/CrabGuiMesh.cpp
@@ -0,0 +1,72 @@
+﻿
+#include "CrabGuiDefine.h"
+#include "CrabGuiMesh.h"
+
+
+namespace CrabGui
+{
+
+	Mesh::Mesh()
+		: _uVertexNum(0), _pVertices(0), _pTexVertices(0)
+		, _uTriangleNum(0), _pTriangles(0)
+	{
+	}
+
+	Mesh::Mesh(UInt uVertexNum, UInt uTriangleNum)
+	{
+		setVertexNum(uVertexNum);
+		setTriangleNum(uTriangleNum);
+	}
+
+	Mesh::~Mesh()
+	{
+		clear();
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	// 顶点数据相关
+
+
+	// 清除数据
+	void Mesh::clear()
+	{
+
+	}
+
+
+	// 获取点个数
+	UInt Mesh::getVertexNum()
+	{
+		return _uVertexNum;
+	}
+
+
+	// 设置点个数
+	void Mesh::setVertexNum(UInt uVertexNum)
+	{
+
+	}
+
+
+	// 添加点
+	Bool Mesh::setVertex(UInt uVertexIndex, const PointReal* pPoint, const PointReal* pTexPoint1, const PointReal* pTexPoint2)
+	{
+		return False;
+	}
+
+
+	// 设置三角形个数
+	void Mesh::setTriangleNum(UInt uTriangleNum)
+	{
+
+	}
+
+
+	// 设置单个三角形
+	Bool Mesh::setTriangle(UInt uTriangleIndex, Int16 sPoint1, Int16 sPoint2, Int16 sPoint3)
+	{
+		return False;
+	}
+
+}
